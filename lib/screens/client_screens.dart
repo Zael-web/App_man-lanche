d1dee5290de071bf195e6ee5622831db8be4bf94
Zael-web <zaelmelo71@gmail.com>
2026-05-22 09:32:16 -0300

    ok

--- lib/screens/client_screens.dart
+++ lib/screens/client_screens.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
-import 'package:mana_lanche/screens/login_screens.dart';
+
 import 'package:mana_lanche/screens/carrinho_screens.dart';
 import 'package:mana_lanche/screens/perfil_screens.dart';
 
